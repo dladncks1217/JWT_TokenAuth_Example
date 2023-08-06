73caa5c9e0ee5a4ef50640674b31e2933d1f8203
dladncks1217 <dlaxodud1217@gmail.com>
2023-08-06 20:16:33 +0900

    feat: 로그인 유지 확인용 getnumber api 추가

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,12 @@ const router = require("express").Router();
 
 router.get("/getuser", authJWT, (req, res, next) => {
   if (req.nick) {
-    res.send({ nick: req.nick, name: req.name, userId: req.id });
+    res.json({ nick: req.nick, name: req.name, userId: req.id });
   }
+});
+
+router.get("/getnumber", authJWT, (_, res) => {
+  res.json({ number: Math.floor(Math.random() * 10) + 1 });
 });
 
 module.exports = router;
